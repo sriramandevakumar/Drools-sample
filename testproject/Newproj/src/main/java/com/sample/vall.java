31d4dc2f778f7d99d3704b8e4bd48fbb2aec1436
sriraman.devakumar <sriraman.devakumar@C17377.intellectdesign.com>
2019-06-06 13:09:01 +0530

    test

---
+++ testproject/Newproj/src/main/java/com/sample/vall.java
@@ -0,0 +1,35 @@
+package com.sample;
+
+import java.util.HashMap;
+
+public class vall {
+	public  static class value{
+    	
+    	public  int a=12;
+    	public  int b=2;
+    	public static  int result=1;
+    	public static HashMap map= new HashMap();
+		public int getA() {
+			return a;
+		}
+		public void setA(int a) {
+			this.a = a;
+		}
+		public int getB() {
+			return b;
+		}
+		public void setB(int b) {
+			this.b = b;
+		}
+		public   int getResult() {
+			return result;
+		}
+		public static void setResult(int result) {
+			value.result = result;
+		}
+    	
+		
+		
+    }
+
+}
